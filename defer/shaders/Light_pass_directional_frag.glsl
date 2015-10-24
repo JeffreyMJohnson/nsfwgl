a2e7265f7c22a051eb4c3bb0f6a65ec4a2409fa0
Jeffrey Johnson <jefjohms@gmail.com>
2015-10-24 12:01:07 -0700

    have ambient and specularity implemented.

--- defer/shaders/Light_pass_directional_frag.glsl
+++ defer/shaders/Light_pass_directional_frag.glsl
@@ -5,24 +5,26 @@ in vec2 vTexCoord;
 out vec3 LightOutput;
 
 //direction in view-space
-uniform vec3 LightDirection = vec3(0,1,0);
-uniform vec3 LightColor = vec3(0,1,0);
+uniform vec3 LightDirection = vec3(0, 1, 0);
+uniform vec3 LightColor = vec3(0, 1, 0);
+uniform vec3 CameraPosition = vec3(0, 0, 10);
+uniform float SpecPower = 128;
 
 uniform sampler2D positionTexture;
 uniform sampler2D normalTexture;
 
-void main() 
+void main()
 {
 	vec3 normal = normalize(texture(normalTexture, vTexCoord).xyz);
 	vec3 position = texture(positionTexture, vTexCoord).xyz;
 
-	float d = max(0, dot( normal, LightDirection)); 
+	float diffuse = max(0, dot(normal, LightDirection));
+	vec3 eye = normalize(CameraPosition - position.xyz);
+	vec3 reflect = reflect(-LightDirection, normal.xyz);
+	float specular = max(0, dot(eye, reflect));
+	specular = pow(specular, SpecPower);
 
-	LightOutput = vec3(LightColor * d);
+	vec3 ambient = vec3(.1f,.1f, 0);
 
-	//vec3 eye = normalize(cameraPosition - vPosition.xyz);
-	//vec3 reflect = reflect(-lightDirection, vNormal.xyz);
-	//float specular = max(0, dot(eye,reflect));
-	//specular = pow(specular, specPower);
-	//FragColor = vec4(diffuse * lightColor + lightColor * specular, 1);
+	LightOutput = vec3(ambient + LightColor * diffuse + LightColor * specular);
 }
