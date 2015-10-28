b0963b48df1ae21a4d45d5f531bd444b9049257c
unknown <jeffrey.johnson@SEA-7FF1BE.sea.aie.edu>
2015-10-28 09:36:52 -0700

    fixed obj bug.

--- defer/shaders/Gpass_vert.glsl
+++ defer/shaders/Gpass_vert.glsl
@@ -1,9 +1,9 @@
 #version 410
 
-layout(location=0) in vec4 Position;
-layout(location=1) in vec4 Normal;
-layout(location=2) in vec4 Tangent;
-layout(location=3) in vec2 TexCoord;
+layout(location = 0) in vec4 Position;
+layout(location = 1) in vec4 Normal;
+layout(location = 2) in vec4 Tangent;
+layout(location = 3) in vec2 TexCoord;
 
 //view-space normal and position
 out vec4 vPosition;
@@ -14,23 +14,11 @@ uniform mat4 Projection;
 uniform mat4 View;
 uniform mat4 Model;
 
-//HACKHACK
-uniform bool isObj = false;
-
-void main() 
+void main()
 {
-	
-	//HACKHACK
-	if (isObj)
-	{
-		vNormal = Model * Normal;
-		vPosition = Model * Position;
-	}
-	else
-	{
-		vNormal = normalize(View * Model * Normal);
-		vPosition = View * Model * Position;
-	}
+
+	vNormal = normalize(View * Model * Normal);
+	vPosition = View * Model * Position;
 	vTexCoord = TexCoord;
 	gl_Position = Projection * View * Model * Position;
 }
