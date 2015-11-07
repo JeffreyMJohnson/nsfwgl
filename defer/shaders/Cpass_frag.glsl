4cc8cd7066f94067c82ac05a4a2093754ac75101
Jeffrey Johnson <jefjohms@gmail.com>
2015-11-07 14:07:26 -0800

    Added debug function for outputting any texture to screen.

--- defer/shaders/Cpass_frag.glsl
+++ defer/shaders/Cpass_frag.glsl
@@ -7,10 +7,21 @@ out vec4 FragColor;
 uniform sampler2D Albedo;
 uniform sampler2D Light;
 
+uniform bool OutputDebugTexture = false;
+uniform sampler2D DebugTexture;
+
 void main() 
 {
 	vec3 light = texture(Light, vTexCoord).rgb;
 	vec3 albedo = texture(Albedo, vTexCoord).rgb;
 	
-	FragColor = vec4(albedo * light, 1);
+	if (OutputDebugTexture)
+	{
+		FragColor = texture(DebugTexture, vTexCoord);
+	}
+	else
+	{
+		FragColor = vec4(albedo * light, 1);
+	}
+	
 }
